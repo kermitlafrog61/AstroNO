2dc551ab5716f028407872d1e8d71aac030f4c3f
kermitlafrog61 <islam.alybaev61@gmail.com>
2025-10-16 17:49:33 +0500

    actions

--- src/core/admin.py
+++ src/core/admin.py
@@ -9,4 +9,9 @@ class RegistrationAdmin(admin.ModelAdmin):
     list_filter = ["status", 'event']
     search_fields = ['student_name', 'student_id']
 
+    def make_published(modeladmin, request, queryset):
+        queryset.update(status="c")
+
+    actions = [make_published]
+
 admin.site.register([Event])
